0fb3232864fa159263010a5fc5b8418a97ae0456
Elton Fernandes <cfthody@hotmail.com>
2024-05-02 07:40:01 +0100

    Fix bug #5 - Show the preselect difficulty in dropdown - Remove unused prop

--- src/Components/ListboxSelector/index.tsx
+++ src/Components/ListboxSelector/index.tsx
@@ -1,13 +1,12 @@
 import { Listbox, Transition } from "@headlessui/react";
 import { Fragment } from "react";
+import IconCheckMark from "../Icons/IconCheckMark";
 import IconChevronUpDown from "../Icons/IconChevronUpDown";
 import { ListBoxProps } from "./types";
-import IconCheckMark from "../Icons/IconCheckMark";
 
 function ListboxSelector({
   value,
   onChange,
-  buttonTitle,
   options,
   className,
 }: ListBoxProps) {
@@ -15,7 +14,7 @@ function ListboxSelector({
     <Listbox value={value} onChange={onChange}>
       <div className={`relative mt-1 ${className}`}>
         <Listbox.Button className="relative capitalize w-full rounded-lg border-2 border-yellow-500 hover:border-yellow-600 cursor-pointer py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-yellow-700 focus-visible:ring-2 focus-visible:ring-white/75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-          <span className="block truncate text-white">{buttonTitle}</span>
+          <span className="block truncate text-white">{value}</span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
             <IconChevronUpDown
               className="h-5 w-5 text-gray-400"
